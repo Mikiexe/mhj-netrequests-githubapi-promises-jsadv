549689a08325e8c2a785bb31596fd4162f88cdc5
Mikiexe <matias06112001@gmail.com>
2022-11-08 18:11:24 +0100

    Loading added per input item

--- js/retrieveData.js
+++ js/retrieveData.js
@@ -5,22 +5,35 @@ let resultDiv = document.getElementById('queryResults');
 console.log(retrieveBtn);
 
 retrieveBtn.addEventListener('click', () => {
-  // console.log(userNames);
+  //Loop For necesario para recorrer cada petición
   for (let i = 0; i < userNames.length; i++) {
+    // Creación de los estados de las peticiones para cada input
+    let newLoadingElmnt = document.createElement('p');
+    newLoadingElmnt.innerHTML = 'loading...';
+    newLoadingElmnt.className = 'loadingElement';
+    resultDiv.appendChild(newLoadingElmnt);
+
     if (userNames[i].value == null || userNames[i].value == '') {
       console.log('usuario no definido');
     } else {
-      fetch(`https://api.github.com/users/${userNames[i].value}`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.avatar_url);
-          let elementAdd = data.avatar_url;
-          let newLink = document.createElement('a');
-          let textInside = `${userNames[i].value} avatar url`;
-          newLink.append(textInside);
-          newLink.href = elementAdd;
-          resultDiv.appendChild(newLink);
-        });
+      let promiseLoading = new Promise((resolve, myReject) => {
+        //Fetch a la API, selección y guardado de datos y  generación de respuesta en el DOM
+        fetch(`https://api.github.com/users/${userNames[i].value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data.avatar_url);
+            let elementAdd = data.avatar_url;
+            let newLink = document.createElement('a');
+            let textInside = `${userNames[i].value} avatar url`;
+            newLink.append(textInside);
+            newLink.href = elementAdd;
+            resultDiv.appendChild(newLink);
+          });
+      });
+      promiseLoading.then(function (result) {
+        console.log('hey');
+        newLoadingElmnt.remove();
+      });
     }
   }
 });
